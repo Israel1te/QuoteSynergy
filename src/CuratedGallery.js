f79a2f57a2e6e10bc70f6bcc082c8db65c91628a
asl87 <levanti.arman@gmail.com>
2024-04-15 21:22:28 -0500

    Modify the CuratedContent tab to be responsive, add video titles to videos.json to show the titles below the thumbnails in the carosuel.

--- src/CuratedGallery.js
+++ src/CuratedGallery.js
@@ -1,12 +1,9 @@
-// TODO: Refine webpage CSS to make it look more appealing/modern
-// TODO: Investigate unintended carousel behavior caused by window resizing
-
-import axios from 'axios'; // for making HTTP requests
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
-import Slider from 'react-slick'; // Import react-slick
-import 'slick-carousel/slick/slick-theme.css'; // Import slick carousel theme CSS
-import 'slick-carousel/slick/slick.css'; // Import slick carousel CSS
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const PodcastCarousel = ({ podcasts, onPodcastClick }) => {
   const settings = {
@@ -17,6 +14,12 @@ const PodcastCarousel = ({ podcasts, onPodcastClick }) => {
     slidesToScroll: 1,
     autoplay: true,
     responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
       {
         breakpoint: 768,
         settings: {
@@ -37,9 +40,9 @@ const PodcastCarousel = ({ podcasts, onPodcastClick }) => {
       {podcasts.map((podcast) => (
         <div key={podcast.id} onClick={() => onPodcastClick(podcast)} style={styles.podcastItem}>
           <img src={podcast.thumbnail} alt={podcast.title} style={styles.podcastThumbnail} />
-          <p style={styles.podcastTitle}>{podcast.title}</p> {/* Apply podcastTitle style */}
+          <p style={styles.podcastTitle}>{podcast.title}</p>
         </div>
-))}
+      ))}
     </Slider>
   );
 };
@@ -53,8 +56,35 @@ const CuratedGallery = () => {
   const [isVideoVisible, setIsVideoVisible] = useState(false);
   const [isPodcastVisible, setIsPodcastVisible] = useState(false);
 
+  const carouselSettings = {
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: 'linear',
+    pauseOnHover: false,
+    swipeToSlide: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   useEffect(() => {
-    // Function to fetch video data from JSON file
     const fetchVideoData = async () => {
       try {
         const response = await axios.get('/curated-content/videos.json');
@@ -66,7 +96,6 @@ const CuratedGallery = () => {
 
     fetchVideoData();
 
-    // Function to fetch podcast data from JSON file
     const fetchPodcasts = async () => {
       try {
         const response = await axios.get('/curated-content/podcasts.json');
@@ -105,14 +134,12 @@ const CuratedGallery = () => {
 
   return (
     <div style={styles.container}>
-      {/* Title Container */}
       <div style={styles.titleContainer}>
         <h1 style={styles.title}>Curated Content Gallery</h1>
         <h4 style={styles.subtitle}>Hand selected with care by our site's administrators!</h4>
         <h4 style={styles.subtitle}>~New videos and podcasts updated every week~</h4>
       </div>
 
-      {/* Category Toggle */}
       <div style={styles.toggleContainer}>
         <label style={styles.toggleLabel}>
           <input
@@ -143,7 +170,6 @@ const CuratedGallery = () => {
         </label>
       </div>
 
-      {/* Selected video */}
       {selectedVideo && (
         <div style={{ ...styles.videoContainer, opacity: isVideoVisible ? 1 : 0 }}>
           <button style={styles.closeButton} onClick={handleCloseVideo}>X</button>
@@ -156,40 +182,34 @@ const CuratedGallery = () => {
         </div>
       )}
 
-      {/* YouTube Carousel */}
       <div style={styles.carouselContainer}>
         <h2 style={styles.carouselHeader}>Featured Videos</h2>
         {videoData && (
           <Slider {...carouselSettings}>
-            {videoData[selectedCategory].map((videoUrl, index) => (
+            {videoData[selectedCategory].map((video, index) => (
               <div key={index} style={styles.thumbnailContainer}>
                 <img
-                  src={`https://img.youtube.com/vi/${videoUrl.split('v=')[1]}/0.jpg`}
+                  src={`https://img.youtube.com/vi/${video.url.split('v=')[1]}/0.jpg`}
                   alt={`Thumbnail ${index}`}
-                  onClick={() => handleClick(videoUrl)}
-                  style={{
-                    ...styles.thumbnail,
-                    width: '200px', // Set a fixed width for the thumbnails (adjust as needed)
-                    height: 'auto', // Maintain aspect ratio
-                    margin: '10px',
-                    padding: '5px'
-                  }}
+                  onClick={() => handleClick(video.url)}
+                  style={styles.thumbnail}
                 />
+                <p style={styles.thumbnailTitle}>
+                  {video.title.length > 40 ? `${video.title.substring(0, 40)}...` : video.title}
+                </p>
               </div>
             ))}
           </Slider>
         )}
       </div>
 
-        {/* Selected podcast */}
-        {selectedPodcast && (
+      {selectedPodcast && (
         <div style={{ ...styles.selectedPodcastContainer, opacity: isPodcastVisible ? 1 : 0 }}>
           <button style={styles.closeButton} onClick={handleClosePodcast}>X</button>
           <div dangerouslySetInnerHTML={{ __html: selectedPodcast.embedCode }} />
         </div>
       )}
 
-      {/* Podcast Carousel */}
       <div style={styles.podcastCarouselWrapper}>
         <h2 style={styles.carouselHeader}>Featured Podcasts</h2>
         {podcasts.length > 0 && (
@@ -202,62 +222,27 @@ const CuratedGallery = () => {
 
 export default CuratedGallery;
 
-// YouTube Carousel Preferences
-const carouselSettings = {
-  dots: false,
-  infinite: true,
-  speed: 3000,
-  slidesToShow: 3, // Adjust the number of thumbnails displayed
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 0,
-  cssEase: 'linear',
-  pauseOnHover: false,
-  swipeToSlide: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
-};
-
 const styles = {
   container: {
-    position: 'relative',
-    top: "100px",
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    border: '2px solid #ccc',
     padding: '20px',
-    borderRadius: '8px',
     fontFamily: 'Arial, sans-serif',
   },
   titleContainer: {
-    position: 'absolute',
-    top: '-65px',
-    left: '20px',
     backgroundColor: '#fff',
     padding: '10px',
     borderRadius: '8px',
     boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    marginBottom: '20px',
   },
   title: {
     fontSize: '24px',
     fontWeight: 'bold',
     margin: '0',
     color: '#333',
-    textDecoration: 'underline'
+    textDecoration: 'underline',
   },
   subtitle: {
     fontSize: '16px',
@@ -269,6 +254,8 @@ const styles = {
     border: '1px solid #ccc',
     padding: '10px',
     borderRadius: '4px',
+    display: 'flex',
+    alignItems: 'center',
   },
   toggleLabel: {
     marginRight: '10px',
@@ -282,9 +269,9 @@ const styles = {
     textAlign: 'center',
     backgroundColor: '#f7f7f7',
     borderRadius: '8px',
-    padding: '20px 20px 50px 20px',
-    opacity: '0', // Set initial opacity to 0
-    transition: 'opacity 1s ease-in-out', // Add transition for opacity
+    padding: '20px',
+    opacity: '0',
+    transition: 'opacity 1s ease-in-out',
   },
   carouselContainer: {
     width: '100%',
@@ -294,64 +281,10 @@ const styles = {
     borderRadius: '8px',
     outline: 'none',
   },
-  podcastCarouselWrapper: {
-    width: '100%',
-    maxWidth: '800px',
-    padding: '40px',
-    backgroundColor: '#f7f7f7',
-    borderRadius: '8px',
-    marginTop: '20px',
-  },
-  podcastCarouselContainer: {
-    display: 'flex',
-    overflowX: 'auto',
-    padding: '10px 0',
-    margin: '0 -10px',
-  },
-  podcastItem: {
-    flex: '0 0 auto',
-    margin: '0 10px',
-    textAlign: 'center', // Center align the podcast items
-  },
-  podcastThumbnail: {
-    maxWidth: '150px',
-    maxHeight: '150px',
-    width: 'auto',
-    height: 'auto',
-    cursor: 'pointer',
-    textAlign: 'center',
-    margin: '0 auto', // Center align the thumbnail
-  },
-  podcastTitle: {
-    textAlign: 'center', // Center align the wrapped text
-    wordWrap: 'break-word', // Allow the title to wrap
-  },
-  selectedPodcastContainer: {
-    margin: '20px',
-    padding: '15px 15px 15px 15px',
-    width: '100%',
-    maxWidth: '800px',
-    height: '350px',
-    textAlign: 'center',
-    backgroundColor: '#f7f7f7',
-    borderRadius: '8px',
-    opacity: '0', // Set initial opacity to 0
-    transition: 'opacity 1s ease-in-out', // Add transition for opacity
-  },
-  carouselHeader: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    marginBottom: '20px',
-    textAlign: 'center',
-    color: '#333',
-    textDecoration: 'underline',
-  },
   thumbnailContainer: {
-    width: '100%',
     display: 'flex',
-    justifyContent: 'center',
-    margin: '10px',
-    padding: '5px',
+    flexDirection: 'column',
+    alignItems: 'center',
     outline: 'none',
   },
   thumbnail: {
@@ -360,19 +293,70 @@ const styles = {
     boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
     transition: 'transform 0.3s ease',
     objectFit: 'cover',
-    width: '150px',
+    width: '200px',
     height: '150px',
+    marginBottom: '10px',
     outline: 'none',
   },
+  thumbnailTitle: {
+    textAlign: 'center',
+    margin: '0',
+    fontSize: '14px',
+    color: '#333',
+    width: '200px',
+    overflow: 'hidden',
+    display: '-webkit-box',
+    WebkitLineClamp: 2, // Limit text to two lines
+    WebkitBoxOrient: 'vertical', // Display text vertically
+    lineHeight: '1.2', // Set the line height
+    outline: 'none', // Remove outline on focus
+  },
+  selectedPodcastContainer: {
+    margin: '20px',
+    padding: '15px',
+    width: '100%',
+    maxWidth: '800px',
+    height: '350px',
+    textAlign: 'center',
+    backgroundColor: '#f7f7f7',
+    borderRadius: '8px',
+    opacity: '0',
+    transition: 'opacity 1s ease-in-out',
+  },
+  podcastCarouselWrapper: {
+    width: '100%',
+    maxWidth: '800px',
+    padding: '40px',
+    backgroundColor: '#f7f7f7',
+    borderRadius: '8px',
+    marginTop: '20px',
+  },
+  podcastThumbnail: {
+    maxWidth: '150px',
+    maxHeight: '150px',
+    width: 'auto',
+    height: 'auto',
+    cursor: 'pointer',
+    textAlign: 'center',
+    margin: '0 auto',
+  },  
+  carouselHeader: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+    textAlign: 'center',
+    color: '#333',
+    textDecoration: 'underline',
+  },
   closeButton: {
-    position: 'relative',
-    top: '-10px',
-    right: '-50%',
     cursor: 'pointer',
     backgroundColor: 'transparent',
     border: 'none',
     fontSize: '16px',
     fontWeight: 'bold',
     color: '#333',
+    position: 'absolute',
+    top: '10px',
+    right: '10px',
   },
 };
